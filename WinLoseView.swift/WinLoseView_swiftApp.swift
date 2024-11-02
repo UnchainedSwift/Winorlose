55c0f02a82cf55bbf2b544475f4469cb7c5a7b15
Victoria Mckinnie <Vmcki004@fiu.edu>
2024-11-01 23:18:45 -0400

    Initial Commit

---
+++ WinLoseView.swift/WinLoseView_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinLoseView_swiftApp.swift
+//  WinLoseView.swift
+//
+//  Created by user on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WinLoseView_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
